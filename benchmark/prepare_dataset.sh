b9ee16323d981fcc0ff309f8f303c1edc456954f
Sofi Shulepina <95312480+sofi12321@users.noreply.github.com>
2023-12-03 19:04:26 +0300

    Update prepare_dataset.sh

--- benchmark/prepare_dataset.sh
+++ benchmark/prepare_dataset.sh
@@ -1,10 +1,12 @@
+cd Movie_Recommendations
+
 wget https://files.grouplens.org/datasets/movielens/ml-100k.zip
 unzip ml-100k.zip
-mv ml-100k/ua.base Movie_Recommendations/data/raw
-mv ml-100k/ua.test Movie_Recommendations/data/raw
-mv ml-100k/u.user Movie_Recommendations/data/raw
-mv ml-100k/u.genre Movie_Recommendations/data/raw
-mv ml-100k/u.item Movie_Recommendations/data/raw
+mv ml-100k/ua.base data/raw
+mv ml-100k/ua.test data/raw
+mv ml-100k/u.user data/raw
+mv ml-100k/u.genre data/raw
+mv ml-100k/u.item data/raw
 
 cd data/interim
 gdown https://drive.google.com/uc?id=1VLrWudGAGcQDRHiKDx-T72XfAmU5FUdY
